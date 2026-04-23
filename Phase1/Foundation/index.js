f76c310fc3cf889084c1427bea848c2855e6ee98
summi-rahman <tahseenanwer100@gmail.com>
2026-04-23 21:09:36 +0530

    Starting Bsckend

---
+++ Phase1/Foundation/index.js
@@ -0,0 +1,12 @@
+// Callback 
+
+function greeting(name ,age){
+    console.log(`Welcome ${name} : ${age} `);
+   saygoodbye();
+}
+
+function saygoodbye(){
+    console.log("See you again good bye ");
+}
+
+greeting("Alic",56)
